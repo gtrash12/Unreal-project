05fdb916524d428bb75397296ac8afca423053b2
민진희_T1251 <gtrash12@naver.com>
2021-04-15 14:50:15 +0900

    damage_data 구조체, 데미지 데이터 테이블
    프로젝트 리빌드, 리셋업, 리세이브
    (source build C++ 생성 문제)
    
    c++로 damage_data 구조체 생성
    damage_data 를 스키마로 사용하는 데이터 테이블 생성
    외부 CSV파일로 테이블 셋업
    공격시 테이블에서 데미지 정보 불러오도록 구현 (데이터 드리븐)

--- Source/CyperArena/CyperArena.Build.cs
+++ Source/CyperArena/CyperArena.Build.cs
@@ -8,7 +8,7 @@ public class CyperArena : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine",  "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
---
+++ Source/CyperArena/DamageDataTable.h
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Engine/DataTable.h"
+
+#include "DamageDataTable.generated.h"
+
+
+
+USTRUCT(Atomic, BlueprintType)
+struct FdamageData : public FTableRowBase
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+		FString name;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+		float base_damage;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+		float base_damage_percent;
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+		FVector knock_back;
+};
+
+UCLASS()
+class CYPERARENA_API ADamageDataTable : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADamageDataTable();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
